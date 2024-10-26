8979df07d3326ffc60aa6a5bab1d470d93b72f11
mariohamann <mario.hamann@virtual-identity.com>
2024-10-26 07:10:53 +0200

    remove svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 
 import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./ui-src",
-  plugins: [svelte(), viteSingleFile()],
+  plugins: [viteSingleFile()],
   build: {
     target: "esnext",
     assetsInlineLimit: 100000000,
